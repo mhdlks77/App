727a7c43b433a6cf7611a5a556f2b38345d64d8f
Saeed <tbhidc1@mail.com>
2024-11-26 16:50:31 -0500

    Updated Settings Activity. Removed extra clutter and moved automation to settings.

--- app/src/main/java/com/example/coen391app/SettingsActivity.java
+++ app/src/main/java/com/example/coen391app/SettingsActivity.java
@@ -3,36 +3,62 @@ package com.example.coen391app;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.SeekBar;
-import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.DialogFragment;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private LinearLayout waterSettingsLayout;
-    private LinearLayout uvSettingsLayout;
+    private DatabaseReference rootDatabaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-
-        waterSettingsLayout = findViewById(R.id.waterSettingsLayout);
-        uvSettingsLayout = findViewById(R.id.uvSettingsLayout);
-        TextView waterSettingsHeader = findViewById(R.id.waterSettingsHeader);
-        TextView uvSettingsHeader = findViewById(R.id.uvSettingsHeader);
         Button wifiSetup_btn = findViewById(R.id.wifi_setup_button);
+        SwitchCompat wateringSwitch = findViewById(R.id.wateringSwitch);
+
+        //Setup Toolbar with back navigation
+        Toolbar settings_toolbar = findViewById(R.id.settings_toolbar);
+        setSupportActionBar(settings_toolbar);
+
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        // Initialize Firebase Database reference
+        rootDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        //Watering Switch
+        wateringSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            rootDatabaseRef.child("Automation").child("Watering").setValue(isChecked)
+                    .addOnSuccessListener(aVoid -> Log.d("DataActivity", "Watering updated to " + isChecked))
+                    .addOnFailureListener(e -> Log.w("DataActivity", "Failed to update Watering", e));
+        });
+
+
+        //Wifi Credentials Setup
         wifiSetup_btn.setOnClickListener(v -> {
             // Check for ACCESS_FINE_LOCATION permission
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -48,33 +74,5 @@ public class SettingsActivity extends AppCompatActivity {
 
         });
 
-
-
-        // Water Settings
-        waterSettingsHeader.setOnClickListener(view -> {
-            if (waterSettingsLayout.getVisibility() == View.VISIBLE) {
-                waterSettingsLayout.setVisibility(View.GONE);
-            } else {
-                waterSettingsLayout.setVisibility(View.VISIBLE);
-            }
-        });
-
-        // UV
-        uvSettingsHeader.setOnClickListener(view -> {
-            if (uvSettingsLayout.getVisibility() == View.VISIBLE) {
-                uvSettingsLayout.setVisibility(View.GONE);
-            } else {
-                uvSettingsLayout.setVisibility(View.VISIBLE);
-            }
-        });
-
-
-        SeekBar lowSoilMoistureSlider = findViewById(R.id.lowSoilMoistureSlider);
-        SeekBar wateringSlider = findViewById(R.id.wateringSlider);
-        Switch wateringSwitch = findViewById(R.id.wateringSwitch);
-        SeekBar uvSlider = findViewById(R.id.uvSlider);
-        Switch themeSwitch = findViewById(R.id.themeSwitch);
-
-        // ADD ADDITIONAL LOGIC HERE
     }
 }
